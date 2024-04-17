ea6b7b18ee79d11be89f2aeff590fcdfc42ebc62
Brunno Viegas <brunnofabricio70@gmail.com>
2024-04-17 01:08:20 -0300

    alteração página login e endereço

---
+++ js/login.js
@@ -0,0 +1,27 @@
+async function sendForm(form) {
+    try {
+        const response = await fetch("model/login.php", { method: 'post', body: new FormData(form) });
+        if (!response.ok) throw new Error(response.statusText);
+        var bodyText = await response.text();
+        const result = JSON.parse(bodyText);
+
+        if (result.success)
+            window.location = result.detail;
+        else {
+            document.querySelector("#loginFailMsg").style.display = 'block';
+            form.senha.value = "";
+            form.senha.focus();
+        }
+    }
+    catch (e) {
+        console.log(bodyText ?? "");
+        console.error(e);
+    }
+
+}
+const form = document.querySelector("#formulario");
+form.onsubmit = function (e) {
+    sendForm(form);
+    e.preventDefault();
+
+}
---
+++ model/login.php
@@ -0,0 +1,63 @@
+<?php
+
+require "../conexaoBanco.php";
+
+class RequestResponse
+{
+  public $success;
+  public $detail;
+
+  function __construct($success, $detail)
+  {
+    $this->success = $success;
+    $this->detail = $detail;
+  }
+}
+
+function checkLogin($pdo, $email, $senha)
+{ 
+  $sql = <<<SQL
+    SELECT senhaHash
+    FROM Pessoa
+    INNER JOIN Funcionario
+    ON Pessoa.id_pessoa = Funcionario.id_pessoa
+    WHERE email = ?
+    SQL;
+
+    try {
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute([$email]);
+      $senhaHash = $stmt->fetchColumn();
+  
+      if (!$senhaHash) 
+        return false; // a consulta não retornou nenhum resultado (email não encontrado)
+  
+      if (!password_verify($senha, $senhaHash))
+        return false; // senha incorreta
+        
+      // email e senha corretos
+      return true;
+    } 
+    catch (Exception $e) {
+      exit('Falha inesperada: ' . $e->getMessage());
+    }
+}
+
+$user = $_POST["user"] ?? "";
+$senha = $_POST["senha"] ?? "";
+
+$pdo = mysqlConnect();
+if (checkLogin($pdo, $user, $senha)) { // verifica o retorno da função 
+  $cookieParams = session_get_cookie_params();
+  $cookieParams['httponly'] = true;
+  session_set_cookie_params($cookieParams);
+
+  // cria uma nova sessão para o usuário
+  session_start();
+  $_SESSION['loggedIn'] = true;
+  $_SESSION['user'] = $user;
+  $response = new RequestResponse(true, 'acessorestrito');
+} else
+  $response = new RequestResponse(false, '');
+
+echo json_encode($response);
