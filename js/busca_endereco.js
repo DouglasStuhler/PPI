083a78c3c8e92b553b7d56d445c7ab9e04b8a994
LayzaDev <layzanauanedev@gmail.com>
2024-04-20 14:01:28 -0300

    separando funcao busca_endereco em um arquivo js

--- js/busca_endereco.js
+++ js/busca_endereco.js
@@ -3,7 +3,7 @@ async function buscaEndereco(cep) {
   if (cep.length != 9) return;
 
   try {
-    let response = await fetch("Endereco.php?cep=" + cep);
+    let response = await fetch("../model/Endereco.php?cep=" + cep);
     if (!response.ok) throw new Error(response.statusText);
     var endereco = await response.json();
   }
@@ -13,9 +13,9 @@ async function buscaEndereco(cep) {
   }
 
   let form = document.querySelector("form");
-  form.rua.value = endereco.rua;
-  form.bairro.value = endereco.bairro;
+  form.logradouro.value = endereco.logradouro;
   form.cidade.value = endereco.cidade;
+  form.estado.value = endereco.estado;
 }
 
 window.onload = function () {
